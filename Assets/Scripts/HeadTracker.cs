ad3bd4c4c907be4f4e5c4a882929b2a34b505afc
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-05-11 13:27:58 +0200

    Added character with golfing animation. First attempt for a moving camera.

---
+++ Assets/Scripts/HeadTracker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTracker : MonoBehaviour {
+
+    public GameObject Head;
+    public GameObject Camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.transform.position = Head.transform.position;
+	}
+}
