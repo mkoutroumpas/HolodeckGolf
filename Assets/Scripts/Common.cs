c28c25e00cc43b55b82c9570963228fc51f8129a
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-06-16 09:30:53 +0200

    Cleanup code.

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Common
+﻿public class Common
 {
     public readonly static string ObjectFilterLiteral = "Ball";
 }
