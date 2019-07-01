26ddf9aed57d34490232fdc2e198e958728ccc01
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-07-01 22:27:41 +0200

    Added RotationLimiter.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,8 +4,6 @@ public class Main : MonoBehaviour {
 
     void Start ()
     {
-        ////QualitySettings.vSyncCount = 0;
-        ///
-        Camera.main.transform.rotation = Quaternion.Euler(0, 90, 0);
+        QualitySettings.vSyncCount = 0;
     }
 }
---
+++ Assets/Scripts/RotationLimiter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RotationLimiter : MonoBehaviour {
+
+    void LateUpdate()
+    {
+        rotateCamera();
+    }
+
+    public float xSensitivity = 400.0f;
+    public float ySensitivity = 400.0f;
+
+    public float yMinLimit = -85.0f;
+    public float yMaxLimit = 85.0f;
+
+    public float xMinLimit = -90.0f;
+    public float xMaxLimit = 90.0f;
+
+    float yRot = 0.0f;
+    float xRot = 0.0f;
+
+    void rotateCamera()
+    {
+        xRot += Input.GetAxis("Mouse X") * xSensitivity * Time.deltaTime;
+        yRot += Input.GetAxis("Mouse Y") * ySensitivity * Time.deltaTime;
+        yRot = Mathf.Clamp(yRot, yMinLimit, yMaxLimit);
+        xRot = Mathf.Clamp(xRot, xMinLimit, xMaxLimit);
+        Camera.main.transform.localEulerAngles = new Vector3(-yRot, xRot, 0);
+    }
+}
