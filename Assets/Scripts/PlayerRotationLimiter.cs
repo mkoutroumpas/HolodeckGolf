a71a2dafaee52563ad954deeb2e49006a41e278c
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-07-12 21:49:12 +0200

    Added FieldOfView property.

--- Assets/Scripts/PlayerRotationLimiter.cs
+++ Assets/Scripts/PlayerRotationLimiter.cs
@@ -14,18 +14,18 @@ public class PlayerRotationLimiter : MonoBehaviour {
     {
         string _rot = "x: " + Camera.main.transform.localEulerAngles.x + ", y: " + Camera.main.transform.localEulerAngles.y + ", z: " + Camera.main.transform.localEulerAngles.z;
 
-        if (!(Camera.main.transform.localEulerAngles.y > 270 && Camera.main.transform.localEulerAngles.y <= 360 ||
-            Camera.main.transform.localEulerAngles.y < 90 && Camera.main.transform.localEulerAngles.y >= 0))  // y: (270 , 90)
+        if (!(Camera.main.transform.localEulerAngles.y > 360 - (FieldOfView / 2) && Camera.main.transform.localEulerAngles.y <= 360 ||
+            Camera.main.transform.localEulerAngles.y < (FieldOfView / 2) && Camera.main.transform.localEulerAngles.y >= 0))  // y: (270 , 90)
         {
             Console.WriteLine(_rot);
 
-            if (Camera.main.transform.localEulerAngles.y <= 270 && Camera.main.transform.localEulerAngles.y > 180)
+            if (Camera.main.transform.localEulerAngles.y <= 360 - (FieldOfView / 2) && Camera.main.transform.localEulerAngles.y > 180)
             {
-                Camera.main.transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, 270, Camera.main.transform.localEulerAngles.z);
+                Camera.main.transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, 360 - (FieldOfView / 2), Camera.main.transform.localEulerAngles.z);
             }
-            if (Camera.main.transform.localEulerAngles.y < 180 && Camera.main.transform.localEulerAngles.y >= 90)
+            else if (Camera.main.transform.localEulerAngles.y < 180 && Camera.main.transform.localEulerAngles.y >= FieldOfView / 2)
             {
-                Camera.main.transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, 90, Camera.main.transform.localEulerAngles.z);
+                Camera.main.transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, FieldOfView / 2, Camera.main.transform.localEulerAngles.z);
             }
         }
     }
