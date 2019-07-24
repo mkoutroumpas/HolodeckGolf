9b363852ac62fdae3cbdb7915bb2aada23d198d6
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-07-24 08:04:39 +0200

    Alternative way to rotate Camera on mobile.

--- Assets/Scripts/PlayerRotationLimiter.cs
+++ Assets/Scripts/PlayerRotationLimiter.cs
@@ -50,11 +50,11 @@ public class PlayerRotationLimiter : MonoBehaviour {
         {
             if (angle < _fullCircle - (fov / 2) && angle > _fullCircle / 2 && Camera.main.transform.forward.z > 0)
             {
-                Camera.main.transform.localEulerAngles = rotateBy;
+                Camera.main.transform.Rotate(rotateBy);
             }
             else if (angle < _fullCircle / 2 && angle > fov / 2 && Camera.main.transform.forward.z < 0)
             {
-                Camera.main.transform.localEulerAngles = rotateByRev;
+                Camera.main.transform.Rotate(rotateByRev);
             }
         }
     }
