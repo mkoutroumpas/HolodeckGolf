c5e8622084109d7f93a6f5e4ee53b0d853195d14
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-07-26 07:02:11 +0200

    Add PlayerRotationLocker.

---
+++ Assets/Scripts/PlayerRotationLocker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotationLocker : MonoBehaviour {
+
+    private void Start () {
+		
+	}
+	
+	private void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        
+    }
+}
