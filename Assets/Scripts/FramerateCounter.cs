767e8f3a45a13b682212b43a86d825aa876b4de5
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-11-16 10:46:33 +0100

    Modified fps display script.

--- Assets/Scripts/FramerateCounter.cs
+++ Assets/Scripts/FramerateCounter.cs
@@ -1,21 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class FramerateCounter : MonoBehaviour {
 
     public Text text;
-    private float fps, deltaTime;
+    private float count;
 
-    void Start()
+    public IEnumerator Start()
     {
-        deltaTime = 0.0f;
-    }
-
-    void Update()
-    {
-        fps = Mathf.FloorToInt(1.0f / deltaTime);
-        text.text = fps.ToString();
-
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        GUI.depth = 2;
+        while (true)
+        {
+            if (Time.timeScale == 1)
+            {
+                yield return new WaitForSeconds(0.1f);
+                count = (1 / Time.deltaTime);
+                text.text = "FPS :" + (Mathf.Round(count));
+            }
+            else
+            {
+                text.text = "Pause";
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
